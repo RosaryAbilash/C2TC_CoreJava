4b969bd077cc5ff322e31cf889d31e1c7e6d427a
Rosary Abilash <75167373+RosaryAbilash@users.noreply.github.com>
2023-07-28 10:04:29 +0530

    Day_5 Encapsulation

---
+++ CoreJava/src/com/cg/DayFive/Encapsulation/Encapsule.java
@@ -0,0 +1,48 @@
+package com.cg.DayFive.Encapsulation;
+
+public class Encapsule {
+	String name;
+	int age;
+	int salary;
+	long mobile;
+	public Encapsule() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Encapsule(String name, int age, int salary, long mobile) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+		this.mobile = mobile;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public long getMobile() {
+		return mobile;
+	}
+	public void setMobile(long mobile) {
+		this.mobile = mobile;
+	}
+	@Override
+	public String toString() {
+		return "Student Data [Student name=" + name + ",Student age=" + age + ",Student salary=" + salary + ",Student mobile=" + mobile + "]";
+	}
+
+}
---
+++ CoreJava/src/com/cg/DayFive/Encapsulation/TestEncapsule.java
@@ -0,0 +1,14 @@
+package com.cg.DayFive.Encapsulation;
+
+public class TestEncapsule extends Encapsule{
+
+	public static void main(String[] args) {
+		Encapsule s1 = new Encapsule();
+		s1.setName("Rosary");
+		s1.setAge(21);
+		s1.setSalary(54000);
+		s1.setMobile(89253150);
+		System.out.println(s1);
+	}
+
+}
